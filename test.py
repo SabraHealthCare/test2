7812b39536a33d6c6865ebd555867a70eb3ed02d
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-11-08 16:09:20 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1093,12 +1093,12 @@ elif st.session_state["authentication_status"] and st.session_state["operator"]!
 		                                                       Upload_And_Process(uploaded_finance,"Finance")
                 elif BS_separate_excel=="Y":     # Finance/BS are in different excel  
 		    # process Finance 
-                   Total_PL,Total_PL_detail,latest_month=Upload_And_Process(uploaded_finance,"Finance")
+                    Total_PL,Total_PL_detail,latest_month=Upload_And_Process(uploaded_finance,"Finance")
 		    # process BS 
-                   Total_BL,Total_BL_detail,latest_month=Upload_And_Process(uploaded_BS,"BS")
+                    Total_BL,Total_BL_detail,latest_month=Upload_And_Process(uploaded_BS,"BS")
 		    # combine Finance and BS
                     Total_PL=Total_PL.combine_first(Total_BL)
-                    otal_PL_detail=Total_PL_detail.combine_first(Total_BL_detail)
+                    Total_PL_detail=Total_PL_detail.combine_first(Total_BL_detail)
 			
                 diff_BPC_PL,diff_BPC_PL_detail,percent_discrepancy_accounts=Compare_PL_Sabra(Total_PL,Total_PL_detail)
        
