b32a93f105a42fee41c9361c2bf6a5295dd899be
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-11-10 09:46:35 -0800

    Update test.py

--- test.py
+++ test.py
@@ -29,19 +29,11 @@ st.set_page_config(
 
 placeholder = st.empty()
 
-# Replace the placeholder with some text:
-placeholder.text("Hello")
-
-# Replace the text with a chart:
-placeholder.line_chart({"data": [1, 5, 2, 6]})
-
-# Replace the chart with several elements:
-with placeholder.container():
-    st.write("This is one element")
-    st.write("This is another")
-
-# Clear all those elements:
-placeholder.empty()
+with st.empty():
+    for seconds in range(60):
+        st.write(f"⏳ {seconds} seconds have passed")
+        time.sleep(1)
+    st.write("✔️ 1 minute over!")
 st.title("Sabra HealthCare Monthly Reporting App")
 sheet_name_discrepancy="Discrepancy_Review"
 bucket_mapping="sabramapping"
