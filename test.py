33e15f030c8b498a1f790f923157bba616002d31
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-11-10 10:40:18 -0800

    Update test.py

--- test.py
+++ test.py
@@ -892,10 +892,10 @@ def Read_Clean_PL(entity_i,sheet_type,PL_sheet_list,uploaded_file):
         Update_File_inS3(bucket_mapping,entity_mapping_filename,entity_mapping,operator)    
 
     # Start checking process
-    progress_text = "********Start to check property—'"+property_name+"' in sheet '"+sheet_name+"'********"
+    progress_text = "********Check property—'"+property_name+"' in sheet '"+sheet_name+"'********"
     my_bar = st.progress(0, text=progress_text)
-
-    for percent_complete in range(20):
+    st.snow()
+    for percent_complete in range(50):
         time.sleep(0.01)
         my_bar.progress(percent_complete + 1, text=progress_text)
     my_bar.empty()
