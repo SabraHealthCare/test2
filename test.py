1f31aea273b07bcafe5fa4f222b5bd61803e04d8
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-11-12 11:00:28 -0800

    Update test.py

--- test.py
+++ test.py
@@ -967,7 +967,7 @@ def Check_Reporting_Month(PL):
                 with col4:
                     st.write(2,st.session_state.latest_month)
                     month = st.selectbox('Month', range(1, 13),index=int(st.session_state.latest_month))
-                confirm_month=st.form_submit_button("Submit",on_click=latest_month_clicked, args=month)
+                confirm_month=st.form_submit_button("Submit",on_click=latest_month_clicked, args=[month])
             if confirm_month:
                 if month<10:
                     latest_month=str(year)+"0"+str(month)
