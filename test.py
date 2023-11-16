781274f82786ff959a6680506b513425cbd82a8c
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-11-16 15:14:36 -0800

    Update test.py

--- test.py
+++ test.py
@@ -79,7 +79,7 @@ def Update_File_inS3(bucket,key,new_data,operator,month=None):  # replace origin
 
         if month:
             st.write("operator,month",operator,month,original_data[(original_data['Operator'] == operator)&(original_data['TIME'] == month)])
-            original_data.TIME = df.TIME.astype(str)
+            original_data.TIME = original_data.TIME.astype(str)
 	    # remove original data by operator and month 
             #st.write(original_data[(original_data['Operator'] == operator)&(original_data['TIME'] == month)])
             original_data = original_data.drop(original_data[(original_data['Operator'] == operator)&(original_data['TIME'] == month)].index)
