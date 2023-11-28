0e1fe3a1343505f4946982629149d9feebe10248
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-11-28 14:34:30 -0800

    Update test.py

--- test.py
+++ test.py
@@ -739,12 +739,12 @@ def View_Summary(uploaded_file):
     for i in range(latest_month_data.shape[0]):
         if latest_month_data.loc[i,"Sabra_Account"]=="Total_Sabra" and latest_month_data.loc[i,'Category'] !="Facility Information":
             latest_month_data.loc[i,"Sabra_Account"]="Total - "+latest_month_data.loc[i,'Category']
-        elif latest_month_data.loc[i,"Sabra_Account"]=="Total_Sabra":# and (latest_month_data.loc[i,'Category'] =="Facility Information" or latest_month_data.loc[i,'Category'] =="Additional Statistical Information"):
+        #elif latest_month_data.loc[i,"Sabra_Account"]=="Total_Sabra":# and (latest_month_data.loc[i,'Category'] =="Facility Information" or latest_month_data.loc[i,'Category'] =="Additional Statistical Information"):
             #latest_month_data.loc[i,set_empty]=""
-            latest_month_data.loc[i,"Sabra_Account"]=latest_month_data.loc[i,'Category']
+            #latest_month_data.loc[i,"Sabra_Account"]=latest_month_data.loc[i,'Category']
     st.write(1,latest_month_data)
-    drop_facility_info_total=latest_month_data["Sabra_Account"] == 'Total_Sabra'
-    latest_month_data=latest_month_data[~drop_facility_info_total]
+    #drop_facility_info_total=latest_month_data["Sabra_Account"] == 'Total_Sabra'
+    #latest_month_data=latest_month_data[~drop_facility_info_total]
     st.write(2,latest_month_data)
 
 
