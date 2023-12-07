89a5b8e98d18f895b9377d02c1cd2003b58df9de
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-12-07 11:43:28 -0800

    Update test.py

--- test.py
+++ test.py
@@ -844,7 +844,7 @@ def View_Discrepancy(percent_discrepancy_accounts):
 	# insert comments to diff_BPC_PL
         st.write(diff_BPC_PL)
         st.write(diff_BPC_PL.index)
-        diff_BPC_PL=pd.merge(diff_BPC_PL,edited_diff_BPC_PL[["ENTITY","TIME","Type comments below"]],on=["Property_Name","TIME"],how="left")
+        diff_BPC_PL=pd.merge(diff_BPC_PL,edited_diff_BPC_PL[["Property_Name","TIME","Type comments below"]],on=["Property_Name","TIME"],how="left")
         # insert dim to diff_BPC_PL
         diff_BPC_PL=diff_BPC_PL.merge(entity_mapping[["ENTITY","GEOGRAPHY","LEASE_NAME","FACILITY_TYPE","INV_TYPE"]],on="ENTITY",how="left")
         diff_BPC_PL["TIME"]=diff_BPC_PL["TIME"].apply(lambda x: "{}.{}".format(str(x)[0:4],month_abbr[int(str(x)[4:6])]))
