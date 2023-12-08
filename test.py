4a1f6259297264bee61302f606665efc518ee456
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-12-08 11:55:34 -0800

    Update test.py

--- test.py
+++ test.py
@@ -125,9 +125,9 @@ def Initial_Mapping(operator):
     account_mapping=account_mapping[["Operator","Sabra_Account","Sabra_Second_Account","Tenant_Account","Tenant_Formated_Account","Conversion"]] 
     # read property mapping
     entity_mapping=Read_CSV_FromS3(bucket_mapping,entity_mapping_filename)
-    test=entity_mapping.loc[entity_mapping["Operator"]==operator]
+    st.write(entity_mapping.loc[entity_mapping["Operator"]==operator])
     #test=test.set_index("ENTITY")
-    st.write("test",test)
+    #st.write("test",test)
 
     return entity_mapping,account_mapping
 
