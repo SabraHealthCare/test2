5edbaeff1d9eb546694eba0316a9f5f281879f1a
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2023-12-08 11:44:45 -0800

    Update test.py

--- test.py
+++ test.py
@@ -127,7 +127,7 @@ def Initial_Mapping(operator):
     entity_mapping =Read_CSV_FromS3(bucket_mapping,entity_mapping_filename)
     st.write("$$$$$$$$$$$",entity_mapping)
     #st.write(entity_mapping.loc[entity_mapping["Operator"].isin([operator])])
-    test=entity_mapping.loc[entity_mapping["Operator"]=operator]
+    test=entity_mapping.loc[entity_mapping["Operator"]==operator]
     #entity_mapping=entity_mapping.set_index("ENTITY")
     st.write("entity_mapping")
     st.write(test)
