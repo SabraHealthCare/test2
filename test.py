4fc0c93381a63154e124dc9b9c2238dcc83e4ff9
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-01-08 09:46:43 -0800

    Update test.py

--- test.py
+++ test.py
@@ -20,8 +20,7 @@ from st_aggrid import AgGrid, GridUpdateMode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 import requests
 from msal import ConfidentialClientApplication
-from ms_graph import generate_access_token,GRAPH_API_ENDPOINT
-import json
+#from ms_graph import generate_access_token,GRAPH_API_ENDPOINT
 s3 = boto3.client('s3')
 
 #---------------------------define parameters--------------------------
