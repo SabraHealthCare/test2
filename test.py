500e330e3b26df6c05ab705458549aafd9ea7573
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-01-25 09:42:42 -0800

    Update test.py

--- test.py
+++ test.py
@@ -823,7 +823,7 @@ def View_Summary():
     st.markdown("{} {}/{} reporting data:".format(operator,latest_month[4:6],latest_month[0:4]))      
     #st.markdown(latest_month_data.style.set_table_styles(styles).apply(highlight_total,axis=1).map(left_align)
     #		.format(precision=0,thousands=",").hide(axis="index").to_markdown().to_html(),unsafe_allow_html=True)
-    styled_table = (latest_month_data.style.set_table_styles(styles).apply(highlight_total, axis=1).format(precision=0, thousands=",").hide(axis="index").to_markdown().to_html(escape=False)  # Use escape=False to allow HTML tags
+    styled_table = (latest_month_data.style.set_table_styles(styles).apply(highlight_total, axis=1).format(precision=0, thousands=",").hide(axis="index").to_html(escape=False)  # Use escape=False to allow HTML tags
 )
 
     # Display the HTML using st.markdown
