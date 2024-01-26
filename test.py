7c5c6ee5e51bb2da37b2a6bf71d96d5c34c61312
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-01-26 10:51:13 -0800

    Update test.py

--- test.py
+++ test.py
@@ -59,16 +59,17 @@ token_response = app.acquire_token_for_client(scopes=["https://graph.microsoft.c
 access_token = token_response['access_token']
 headers = {'Authorization': 'Bearer ' + access_token,}    
 
-#directly save the uploaded .xlsx file to onedrive
+#directly save the uploaded (.xlsx) file to onedrive
 def Upload_to_Onedrive(uploaded_file,path,file_name):
     try:
         # Set the API endpoint and headers
         api_url = f'https://graph.microsoft.com/v1.0/users/{user_id}/drive/items/root:/{path}/{file_name}:/content'
         # Make the request to upload the file
         response = requests.put(api_url, headers=headers, data=BytesIO(uploaded_file.read()))
+        st.write("success")
         return True
     except:
-        st.write("")# error log
+        st.write("error")# error log
         return False
 
 # no cache read csv/excel from onedrive , return dataframe
