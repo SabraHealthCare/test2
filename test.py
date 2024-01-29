c60edc103ece94684e5599ec90546285ce87b28a
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-01-29 11:08:38 -0800

    Update test.py

--- test.py
+++ test.py
@@ -114,6 +114,8 @@ def Update_File_Onedrive(path,file_name,new_data,operator,value_name=False):  # 
     if True:
         original_data =Read_CSV_From_Onedrive(path,file_name)
         st.write("original_data",original_data)
+        st.write("original_data",original_data.columns)
+        st.write("original_data",new_data.columns)
         original_data=original_data.loc[new_data.columns,:]
         empty_file=False
 
