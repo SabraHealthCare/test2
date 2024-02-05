6db5c3e13e2a516cda325628c1b3472f66574afc
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-02-05 11:36:58 -0800

    Update test.py

--- test.py
+++ test.py
@@ -704,6 +704,7 @@ def Map_PL_Sabra(PL,entity):
     st.write("Before",PL)	
     #PL.dropna(subset=['Sabra_Account'],inplace=True)
     st.write("??",PL['Sabra_Account'])
+	
     st.write("after",PL)
     PL=PL.reset_index(drop=True)
     month_cols=list(filter(lambda x:str(x[0:2])=="20",PL.columns))
