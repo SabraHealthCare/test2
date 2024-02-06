695b22d89950961ad72c56abe144c39ad7ffd3bc
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-02-06 13:52:42 -0800

    Update test.py

--- test.py
+++ test.py
@@ -817,10 +817,9 @@ def View_Summary():
     reported_cagegory=latest_month_data[["Property_Name","Category","ENTITY",latest_month]][latest_month_data["Category"].\
 	    isin(category_list)].groupby(["Property_Name","Category","ENTITY"]).sum().reset_index(drop=False)
     category_property_full_combination = pd.DataFrame(list(product(property_list,category_list)), columns=['Property_Name', 'Category'])
-    st.write(category_property_full_combination)
     missing_category=category_property_full_combination.merge(reported_cagegory,on=['Property_Name', 'Category'],how="left")
     st.write("missing_category",missing_category)
-    missing_category=missing_category[(missing_category[latest_month]==0) | (missing_category[latest_month] is None)]
+    missing_category=missing_category[(missing_category[latest_month]==0) | (missing_category[latest_month].isnull())]
     st.write("missing_category",missing_category)
     if missing_check.shape[0]>0:
         st.error("No data detected for below properties on specific accounts: ")
