46c1eed4a8e05a28281bca4fdfba26e42555af85
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-02-08 10:43:44 -0800

    Update test.py

--- test.py
+++ test.py
@@ -693,7 +693,7 @@ def Map_PL_Sabra(PL,entity):
     main_account_mapping=account_mapping.loc[list(map(lambda x:x==x and x.upper()!='NO NEED TO MAP',account_mapping["Sabra_Account"])),:]
 
     #concat main accounts with second accounts
-    second_account_mapping=account_mapping[account_mapping["Sabra_Second_Account"]!=" " &account_mapping["Sabra_Second_Account"] is not None]
+    second_account_mapping=account_mapping[account_mapping["Sabra_Second_Account"]!=" " ]
     second_account_mapping=second_account_mapping.dropna(subset="Sabra_Second_Account")
     st.write("second_account_mapping---1",second_account_mapping)
     second_account_mapping=account_mapping.loc[(account_mapping["Sabra_Second_Account"]==account_mapping["Sabra_Second_Account"])&(account_mapping["Sabra_Second_Account"]!="NO NEED TO MAP")& (pd.notna(account_mapping["Sabra_Second_Account"]))][["Sabra_Second_Account","Tenant_Formated_Account","Tenant_Account","Conversion"]].\
