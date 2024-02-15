e77f83eeaedcd09bfb4c995d95722fba36bbbd08
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-02-15 15:08:58 -0800

    Update test.py

--- test.py
+++ test.py
@@ -827,7 +827,7 @@ def View_Summary():
     # fill the facility info with historic data
     properties_missing_facility=list(missing_category[missing_category["Category"]=="Facility Information"]["Property_Name"])
     onemonth_before_latest_month=max(list(filter(lambda x: str(x)[0:2]=="20" and str(x)[0:6]!=str(latest_month),BPC_pull.columns)))
-
+    st.write(BPC_pull)
     previous_facility_data=BPC_pull[(BPC_pull["Property_Name"].isin(properties_missing_facility))&(BPC_pull["ACCOUNT"].isin(list(BPC_Account[BPC_Account["Category"]=="Facility Information"]["BPC_Account_Name"])))][onemonth_before_latest_month]
     st.write(previous_facility_data)
         
