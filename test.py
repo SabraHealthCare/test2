377772674de81f4b5d0fa76afc7b0244e8b2d866
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-02-22 10:08:16 -0800

    Update test.py

--- test.py
+++ test.py
@@ -333,6 +333,7 @@ def filters_widgets(df, columns,location="Vertical"):
 
 @st.cache_data
 def Identify_Tenant_Account_Col(PL,sheet_name,sheet_type):
+    st.write("account_mapping",account_mapping) 
     #search tenant account column in P&L, return col number of tenant account	
     account_pool=account_mapping[["Sabra_Account","Tenant_Formated_Account"]].merge(BPC_Account[["BPC_Account_Name","Category"]], left_on="Sabra_Account", right_on="BPC_Account_Name",how="left")	       
     if sheet_type=="Sheet_Name_Finance":
@@ -347,6 +348,7 @@ def Identify_Tenant_Account_Col(PL,sheet_name,sheet_type):
         candidate_col=list(map(lambda x: str(x).strip().upper() if x==x else x,PL.iloc[:,tenantAccount_col_no]))
         st.write("candidate_col",candidate_col)   
         st.write("account_pool",account_pool)    
+   
         #find out how many tenant accounts match with account_pool
         match=[x in candidate_col for x in account_pool]
         st.write(match) 
