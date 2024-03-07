7facb84ee232fb0826d1abc62f2c28d3dd9a9406
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-03-06 18:23:08 -0800

    Update test.py

--- test.py
+++ test.py
@@ -579,14 +579,14 @@ def Identify_Month_Row(PL,tenantAccount_col_no,sheet_name):
                 
             #if there is no year in month header 
             if  year_table.iloc[month_row_index,col_month]==0:
-                st.write("1",year_table.iloc[month_row_index,col_month])
+  
                 #check the next row
                 if month_row_index>0 and month_row_index<PL_row_size and year_table.iloc[month_row_index-1,col_month]!=0 and year_table.iloc[month_row_index+1,col_month]==0:
                     year_table.iloc[month_row_index,col_month]=year_table.iloc[month_row_index-1,col_month]	
-                    st.write("2",year_table.iloc[month_row_index,col_month])
+          
                 elif month_row_index<PL_row_size and year_table.iloc[month_row_index+1,col_month]!=0 :
                     year_table.iloc[month_row_index,col_month]=year_table.iloc[month_row_index+1,col_month]
-                    st.write("3",year_table.iloc[month_row_index,col_month])
+       
                 else:			
                     continue
            
