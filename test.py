4e1c1479bcad533b7fa33dc9f2d4fe0afe9c50c0
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-03-08 09:42:58 -0800

    Update test.py

--- test.py
+++ test.py
@@ -908,8 +908,8 @@ def View_Summary():
     with st.expander("Summary of P&L" ,expanded=True):
         ChangeWidgetFontSize('Summary of P&L', '25px')
         st.write("Reporting months detected in P&L : "+m_str[1:])   
-        st.write("The reporting month is <span style='font-size: 25px;'color: blue; font-weight: bold;'>{}/{}</span>. Reporting data is as below:".format(latest_month[4:6],latest_month[0:4]))
-        st.write("The reporting month is <span style='font-size: 25px; color: blue; font-weight: bold;'>12/23</span>. Reporting data is as below")
+        st.markdown("The reporting month is <span style='font-size: 25px;'color: blue; font-weight: bold;'>{}/{}</span>. Reporting data is as below:".format(latest_month[4:6],latest_month[0:4]),unsafe_allow_html=True))
+
         styled_table = (latest_month_data.replace(0,'').fillna('').style.set_table_styles(styles).apply(highlight_total, axis=1).format(precision=0, thousands=",").hide(axis="index").to_html(escape=False)) # Use escape=False to allow HTML tags
         # Display the HTML using st.markdown
         st.markdown(styled_table, unsafe_allow_html=True)
@@ -1321,7 +1321,7 @@ def Read_Clean_PL_Single(entity_i,sheet_type,PL_sheet_list,uploaded_file):
     return PL,PL_with_detail
 @st.cache_data(experimental_allow_widgets=True) 
 def Check_Latest_Month_Data(latest_month_data):
-    st.write("")
+    
 
 @st.cache_data(experimental_allow_widgets=True) 
 def Check_Reporting_Month(PL):	
