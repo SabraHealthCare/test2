b2532082225d5bc1cfede8ec9627eac76460c73e
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-03-26 16:36:15 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1251,7 +1251,7 @@ def Read_Clean_PL_Multiple(entity_list,sheet_type,PL_sheet_list,uploaded_file):
         st.write("new_entity_header",new_entity_header)
         non_zero_columns = new_entity_header[new_entity_header!= "0"].index
         st.write("non_zero_columns",non_zero_columns)
-        PL = PL[non_zero_columns]    
+        PL = PL.iloc[:,non_zero_columns]    
         PL.columns= [value for value in new_entity_header if value != "0"]
 	    
         #remove rows with nan tenant account
