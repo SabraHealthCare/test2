29d78e67f348d15c4ca95d182aac2c95421e7300
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-03-27 15:01:27 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1241,14 +1241,13 @@ def Read_Clean_PL_Multiple(entity_list,sheet_type,PL_sheet_list,uploaded_file):
             st.error("Fail to identify tenant account column in sheet '{}'".format(sheet_name))
             st.stop()    
 
-        entity_header_row_number,new_entity_header=Identify_Property_Name_Header(PL,entity_list,sheet_name)
-        st.write(4,"PL",PL)   
+        entity_header_row_number,new_entity_header=Identify_Property_Name_Header(PL,entity_list,sheet_name) 
 	#set tenant_account as index of PL
         PL=PL.set_index(PL.iloc[:,tenantAccount_col_no].values)	
-        st.write(5,"PL",PL)   
-	# find the reporting month from 0th row to property header row    
 
+	# find the reporting month from 0th row to property header row    
         reporting_month=Identify_Reporting_Month(PL,entity_header_row_number)  
+        st.write("1",entity_header_row_number,PL)
 	#remove row above property header
         PL=PL.iloc[entity_header_row_number+1:,:]
         st.write(2,"PL",PL)   
