704ca5239a5f4b5e8f684f904d4e70a19bb97dc7
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-03-29 13:07:29 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1169,7 +1169,7 @@ def Identify_Property_Name_Header(PL,entity_list,sheet_name):  # all properties 
         st.error("Can't identify any property name in sheet {}. The property name are supposed to be:{}. Please add and re-upload.".format(sheet_name,",".join(property_name_list_inmapping)))
         st.stop()
     elif len(max_match)>=1:
-        not_match_names = [item for item in property_name_list_in_mapping  if item not in max_match]	         
+        not_match_names = [item.capitalize() for item in property_name_list_in_mapping  if item not in max_match]	         
         st.error("Missing property: **{}** in sheet {}. Please add and re-upload.".format(",".join(not_match_names),sheet_name))
         mapping_dict = {property_name_list_in_mapping[i]: entity_list[i] for i in range(len(property_name_list_in_mapping))}
         mapped_entity = [mapping_dict[property] if property in mapping_dict else "0" for property in header_row]
