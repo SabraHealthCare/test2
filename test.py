e91631e616aa38849cf892cfa6a10b4bd788b3f7
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-04-03 11:18:09 -0700

    Update test.py

--- test.py
+++ test.py
@@ -385,6 +385,9 @@ def Get_Year(single_string):
     return 0,""
 @st.cache_data
 def Get_Month_Year(single_string):
+    if int(single_string)==10:
+        st.write(1,single_string,type(single_string),len(str(int(single_string))))
+	    
     if single_string!=single_string or pd.isna(single_string):
         return 0,0
     elif (type(single_string)==float or type(single_string)==int) and len(str(int(single_string)))<=3:
