2a3cb5f72edd64d86ce6e90782603bb617fc7622
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-04-23 16:06:26 -0700

    Update test.py

--- test.py
+++ test.py
@@ -688,7 +688,7 @@ def Manage_Account_Mapping(new_tenant_account_list,sheet_name):
     Sabra_second_account_list=[np.nan] * count
     Sabra_main_account_value=[np.nan] * count
     Sabra_second_account_value=[np.nan] * count
-    with st.form(key="Mapping_new_accounts"):
+    with st.form(key=new_tenant_account_list[0]):
         for i in range(count):
             st.markdown("## Map **'{}'** in '{}' to Sabra account".format(new_tenant_account_list[i],sheet_name)) 
             col1,col2=st.columns(2) 
