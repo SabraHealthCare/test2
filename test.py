68f2aad982db745fb30a6f9f1a79cad5a49e303a
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-05-08 10:55:35 -0700

    Update test.py

--- test.py
+++ test.py
@@ -505,10 +505,9 @@ def Fill_Facility_Info(missing_category,latest_month):
 	
 @st.cache_data
 def Identify_Month_Row(PL,tenantAccount_col_no,sheet_name,pre_date_header):
-
-    st.write(pre_date_header[0],pre_date_header[1],pre_date_header[2])
     if len(pre_date_header[2])!=0:
         st.write(111)
+        st.write(PL.iloc[pre_date_header[1],:],pre_date_header[2])
         if PL.iloc[pre_date_header[1],:]==pre_date_header[2]:	
             st.write("yess")
             return pre_date_header
