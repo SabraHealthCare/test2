1f0aae79be4bdc037b41e49afe9f9905700b482d
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-05-16 11:46:08 -0700

    Update test.py

--- test.py
+++ test.py
@@ -620,7 +620,7 @@ def Identify_Month_Row(PL,tenantAccount_col_no,sheet_name,pre_date_header):
             else:
                 PL_date_header=year_table.iloc[month_row_index,].apply(lambda x:str(int(x)))+month_table.iloc[month_row_index,].apply(lambda x:"" if x==0 else "0"+str(int(x)) if x<10 else str(int(x))) 
                 st.write(PL_date_header)
-		if latest_month!="0":
+		if latest_month!=="0":
 		    current_date=list(filter(lambda x: x!="00", PL_date_header))[0]
                     st.write(current_date)
                     if current_date!=latest_month:
