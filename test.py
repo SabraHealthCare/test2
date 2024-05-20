aaa5c972c0f0039e057eb2ace72e829ccbcc23e7
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-05-20 16:52:17 -0700

    Update test.py

--- test.py
+++ test.py
@@ -389,7 +389,7 @@ def Get_Month_Year(single_string):
         single_string=single_string.lower()
         Year,Year_keyword=Get_Year(single_string)
     else:
-        st.write("single_string",single_string)	    
+        st.write("single_string",type(single_string),single_string)	    
     # remove year from string, remove days from string
     single_string=single_string.replace(Year_keyword,"").replace("30","").replace("31","").replace("28","").replace("29","").replace("as of","").replace("Actual","")
     
