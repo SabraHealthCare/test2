06e6c81687cd732b1716306c1f0a442c27c3ff14
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-05-24 17:33:09 -0700

    Update test.py

--- test.py
+++ test.py
@@ -789,6 +789,7 @@ def Map_PL_Sabra(PL,entity):
         PL["ENTITY"]=entity	    
          
     elif isinstance(entity, list):  # multiple properties are in one sheet,column name of data is "value" 
+        st.write("1PL",PL)
         monthdays=monthrange(int(str(latest_month)[0:4]), int(str(latest_month)[4:6]))[1]
         for i in range(len(PL.index)):
             conversion=PL.loc[i,"Conversion"]
@@ -797,7 +798,7 @@ def Map_PL_Sabra(PL,entity):
             else:
                 for entity_j in entity:
                     before_conversion=PL.loc[i,entity_j]
-                    if before_conversion!=before_conversion:
+                    if before_conversion!=before_conversion or pd.isna(before_conversion):
                         continue 
                     elif conversion=="/monthdays":	
                         PL.loc[i,entity_j]=before_conversion/monthdays
@@ -805,12 +806,13 @@ def Map_PL_Sabra(PL,entity):
                         PL.loc[i,entity_j]= before_conversion*monthdays
                     elif conversion[0]=="*":
                         PL.loc[i,entity_j]= before_conversion*float(conversion.split("*")[1])
+	st.write("2PL",PL)    
         property_header = [x for x in PL.columns if x not in ["Sabra_Account","Tenant_Account"]]
         PL = pd.melt(PL, id_vars=['Sabra_Account','Tenant_Account'], value_vars=property_header, var_name='ENTITY')     
         PL=PL.drop(["Tenant_Account"], axis=1)
     #PL_with_detail=copy.copy(PL)
     #PL_with_detail=PL_with_detail.set_index(['ENTITY', 'Sabra_Account',"Tenant_Account"])
-    st.write("PL",PL)
+    st.write("3PL",PL)
     # group by Sabra_Account
     PL = PL.groupby(by=['ENTITY',"Sabra_Account"], as_index=True).sum()
     PL= PL.astype(int)    
