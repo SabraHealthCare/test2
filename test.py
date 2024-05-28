e94e20fb2c3c9c8ee06bce1512180a523999e19a
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-05-28 10:10:07 -0700

    Update test.py

--- test.py
+++ test.py
@@ -521,14 +521,13 @@ def Check_Available_Units(check_patient_days,latest_month):
 	
     # search for the Month/year row and return row number
     #entities_missing_facility=list(missing_category[missing_category["Category"]=="Facility Information"]["ENTITY"])
-    onemonth_before_latest_month=max(list(filter(lambda x: str(x)[0:2]=="20" and str(x)[0:6]<str(latest_month),BPC_pull.columns)))
+    onemonth_before_latest_month=max(list(filter(lambda x: str(x)[0:2]=="20" and str(x)<str(latest_month),BPC_pull.columns)))
     BPC_pull_temp=BPC_pull.reset_index(drop=False)
     previous_available_unit=BPC_pull_temp.loc[BPC_pull_temp["Sabra_Account"].isin(availble_unit_accounts),["ENTITY","Property_Name",onemonth_before_latest_month]]  
     previous_available_unit[["ENTITY","Property_Name",onemonth_before_latest_month]].groupby(["Property_Name","ENTITY"]).sum()
     previous_available_unit=previous_available_unit.reset_index(drop=False)[["ENTITY","Property_Name",onemonth_before_latest_month]]
     st.write("previous_available_unit",previous_available_unit)
     check_patient_days=check_patient_days.reset_index(drop=False)
-    st.write("check_patient_days",check_patient_days.loc[check_patient_days['Category'] == 'Operating Beds',])
     st.write(pd.merge(previous_available_unit, check_patient_days.loc[check_patient_days['Category'] == 'Operating Beds',["ENTITY","Property_Name",latest_month]],on=["ENTITY","Property_Name"], how='left'))
 	     
 @st.cache_data
@@ -871,7 +870,7 @@ def Compare_PL_Sabra(Total_PL,latest_month):
             for matrix in BPC_Account.loc[(BPC_Account["Category"]!="Balance Sheet")]["BPC_Account_Name"]: 
             #for matrix in BPC_Account["BPC_Account_Name"]: 
                 try:
-                    BPC_value=int(BPC_pull.loc[entity,matrix][timeid+'00'])
+                    BPC_value=int(BPC_pull.loc[entity,matrix][timeid])
                 except:
                     BPC_value=0
                 try:
