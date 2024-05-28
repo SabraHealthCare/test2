fe1eb61c17c9b5458f5bfdfcfd5b480091f73a41
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-05-28 09:48:37 -0700

    Update test.py

--- test.py
+++ test.py
@@ -526,7 +526,9 @@ def Check_Available_Units(check_patient_days,latest_month):
     previous_available_unit=BPC_pull_temp.loc[BPC_pull_temp["Sabra_Account"].isin(availble_unit_accounts),["ENTITY","Property_Name",onemonth_before_latest_month]]  
     previous_available_unit[["ENTITY","Property_Name",onemonth_before_latest_month]].groupby(["Property_Name","ENTITY"]).sum()
     previous_available_unit=previous_available_unit.reset_index(drop=False)[["ENTITY","Property_Name",onemonth_before_latest_month]]
-    st.write(pd.merge(previous_available_unit, check_patient_days.loc[check_patient_days['Category'] == 'Operating Beds',["ENTITY","Property_Name",latest_month]],on=["ENTITY","Property_Name"], how='left'))
+    st.write("previous_available_unit",previous_available_unit)
+    st.write("check_patient_days",check_patient_days)
+    #st.write(pd.merge(previous_available_unit, check_patient_days.loc[check_patient_days['Category'] == 'Operating Beds',["ENTITY","Property_Name",latest_month]],on=["ENTITY","Property_Name"], how='left'))
 	     
 @st.cache_data
 def Identify_Month_Row(PL,tenantAccount_col_no,sheet_name,pre_date_header):
