60d2dba30153fd7312bfc9f617f9598aad3f29f8
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-05-31 14:06:25 -0700

    Update test.py

--- test.py
+++ test.py
@@ -226,8 +226,8 @@ def Initial_Mapping(operator,latest_month):
         sorted_months = sorted(filtered_months, reverse=True)
         BPC_pull=BPC_pull[sorted_months[:previous_monthes_comparison]]
     
-        entity_mapping.loc[((entity_mapping["DATE_ACQUIRED"]>=int(latest_month))&((pd.isna(entity_mapping["DATE_SOLD_PAYOFF"]))| (entity_mapping["DATE_SOLD_PAYOFF"]<=int(latest_month)))),]
-    st.write(BPC_pull,entity_mapping)
+        entity_mapping.loc[((str(entity_mapping["DATE_ACQUIRED"])>=latest_month)&((pd.isna(entity_mapping["DATE_SOLD_PAYOFF"]))| (str(entity_mapping["DATE_SOLD_PAYOFF"])<=latest_month))),]
+    st.write("BPC_pull",BPC_pull,"entity_mapping",entity_mapping)
     return BPC_pull,entity_mapping,account_mapping
 
 	
