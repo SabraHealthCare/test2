4e48913784272662f22c14c63fb33c833517accf
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-06-03 16:56:35 -0700

    Update test.py

--- test.py
+++ test.py
@@ -537,6 +537,8 @@ def Check_Available_Units(check_patient_days,latest_month):
     if len(problem_properties)>0:
         check_patient_days_display=check_patient_days.loc[(problem_properties,slice(None)),latest_month].reset_index(drop=False)
         check_patient_days_display=check_patient_days_display.pivot_table(index=["Property_Name"],columns="Category", values=latest_month,aggfunc='last')
+        if "Operating Beds" not in check_patient_days_display.columns:
+            check_patient_days_display["Operating Beds"]=0
         st.write("check_patient_days_display",check_patient_days_display)
         st.dataframe(check_patient_days_display.style.map(color_missing, subset=["Patient Days","Operating Beds"]).format(precision=0, thousands=",").hide(axis="index"),
 		    column_config={
