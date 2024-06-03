83dbdc8fa24be2059663143c99f29ad16de3a72b
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-06-02 21:14:51 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1648,7 +1648,7 @@ elif st.session_state["authentication_status"] and st.session_state["operator"]!
                 st.write(entity_mapping)
                 date_header=[[0],0,[]]
                 #if all(entity_mapping["BS_separate_excel"]=="Y"):
-		if False:
+                if False:
                     BS_separate_excel="Y"
                 else:
                     BS_separate_excel="N"
