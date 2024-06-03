28a7c65e9dc3b5729b643dd0cd04824c204ca6a6
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-06-02 21:07:03 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1642,11 +1642,13 @@ elif st.session_state["authentication_status"] and st.session_state["operator"]!
                 with col4:    
                     selected_month = st.selectbox("Month", [str(month).zfill(2) for month in range(1, 13)])
                 latest_month=str(selected_year)+str(selected_month)
+                st.write(latest_month)
                 #st.form_submit_button("Submit",on_click=clicked, args=["submit_reporting_date"])
                 BPC_pull,entity_mapping,account_mapping=Initial_Mapping(operator,latest_month)
                 account_pool=account_mapping[["Sabra_Account","Tenant_Formated_Account"]].merge(BPC_Account[["BPC_Account_Name","Category"]], left_on="Sabra_Account", right_on="BPC_Account_Name",how="left")
                 reporting_month_label=True  
                 tenant_account_col=10000
+                st.write(entity_mapping)
                 date_header=[[0],0,[]]
                 if all(entity_mapping["BS_separate_excel"]=="Y"):
                     BS_separate_excel="Y"
