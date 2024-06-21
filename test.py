65fab5a349f3de1267d01cecf1a38f1d17630c6c
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-06-21 09:52:30 -0700

    Update test.py

--- test.py
+++ test.py
@@ -678,7 +678,7 @@ def Identify_Month_Row(PL,tenantAccount_col_no,sheet_name,pre_date_header):
                     elif count_reporting_month>1:
                         st.error("There are {} '{}/{}' header in sheet '{}'. Only one is allowed to identify the data column of '{}/{}'".\
 			     format(count_reporting_month,reporting_month[4:6],reporting_month[0:4],sheet_name,reporting_month[4:6],reporting_month[0:4]))
-		    elif count_reporting_month==1:  # there is only one reporting month in the header
+                    elif count_reporting_month==1:  # there is only one reporting month in the header
                         return PL_date_header,month_row_index,PL.iloc[month_row_index,:]	
 			
                 else:
