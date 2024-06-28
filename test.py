5227395fe21dcb29759cc2873e590ad3916e6db0
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-06-28 14:38:52 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1378,6 +1378,7 @@ def Identify_Property_Name_Header(PL,entity_list,sheet_name):  # all properties 
         miss_match_names = [item for item in property_name_list_in_mapping  if item not in max_match]
         total_missed_entities=entity_mapping[entity_mapping["Column_Name"].str.upper().str.strip().isin(miss_match_names)].index.tolist()+entity_without_propertynamefinance
         miss_column_mapping=entity_mapping.loc[total_missed_entities]
+        st.write("total_missed_entities",total_missed_entities,"miss_column_mapping",miss_column_mapping)
         column_names=[x for x in PL.iloc[max_match_row,:] if pd.notna(x)]
         st.error("Please map the column names for following facilities in sheet {}.".format(sheet_name))
         with st.form(key="miss_match_column_name"):
@@ -1410,6 +1411,7 @@ def Identify_Property_Name_Header(PL,entity_list,sheet_name):  # all properties 
             mapped_entity = [mapping_dict[property] if property in mapping_dict else "0" for property in header_row]
             # update entity_mapping in onedrive  
             Update_File_Onedrive(mapping_path,entity_mapping_filename,entity_mapping,operator,total_missed_entities,entity_mapping_str_col)
+        
             return row_i,mapped_entity
 
 # no cache
