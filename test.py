b4443f034e106a4b82bba32e9db3d68f521af834
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-07-02 11:07:24 -0700

    Update test.py

--- test.py
+++ test.py
@@ -635,8 +635,8 @@ def Identify_Month_Row(PL,tenantAccount_col_no,sheet_name,pre_date_header):
                         PL_date_header=year_table.iloc[month_row_index,].apply(lambda x:str(int(x)))+\
                                                       month_table.iloc[month_row_index,].apply(lambda x:"" if x==0 else "0"+str(int(x)) if x<10 else str(int(x)))
                         st.write("PL_date_header",PL_date_header)
-                        st.write("instance(reporting_month, (int, float)",instance(reporting_month, (int, float))
-                        st.write("reporting_month not in PL_date_header",reporting_month in PL_date_header)
+                        st.write("instance(reporting_month, (int, float)",instance(reporting_month, (int, float)))
+                        st.write("reporting_month in PL_date_header",reporting_month in PL_date_header)
                         if reporting_month not in PL_date_header:
                             year_table.iloc[month_row_index,]=Fill_Year_To_Header(list(month_table.iloc[month_row_index,]),sheet_name)
                             PL_date_header=year_table.iloc[month_row_index,].apply(lambda x:str(int(x)))+month_table.iloc[month_row_index,].apply(lambda x:"" if x==0 else "0"+str(int(x)) if x<10 else str(int(x)))
