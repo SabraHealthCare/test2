d42361f0a5b9d2c1988e8e42d9aa3b753ebb70f3
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-07-07 07:57:15 -0700

    Update test.py

--- test.py
+++ test.py
@@ -446,7 +446,7 @@ def Fill_Year_To_Header(PL_,month_row_index,full_month_header,sheet_name,reporti
     #remove rows with nan tenant account
     nan_index=list(filter(lambda x:pd.isna(x) or x=="nan" or x=="" or x==" " or x!=x or x==0 ,PL_.index))
     st.write("PL4",PL_)
-    column_mask = [all(val == 0 or isinstance(val, str) or pd_.isna(val) for val in PL_.drop(nan_index).iloc[month_row_index:, i]) for i in range(PL_.drop(nan_index).shape[1])]
+    column_mask = [all(val == 0 or isinstance(val, str) or pd.isna(val) for val in PL_.drop(nan_index).iloc[month_row_index:, i]) for i in range(PL_.drop(nan_index).shape[1])]
     st.write("PL5",PL_)
     # Apply the mask to set these columns to NaN in the row specified by month_row_index
     full_month_header=[0 if column_mask[i] else full_month_header[i] for i in range(len(full_month_header))]
