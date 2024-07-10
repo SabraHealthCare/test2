ddddc0b7688e8199f6f4103ac01a4ce534c69b00
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-07-10 10:43:01 -0700

    Update test.py

--- test.py
+++ test.py
@@ -601,7 +601,7 @@ def Identify_Month_Row(PL,sheet_name,pre_date_header,tenantAccount_col_no):
     for row_i in range(search_row_size):
         for col_i in range(PL_col_size):
             month_table.iloc[row_i,col_i],year_table.iloc[row_i,col_i]=Get_Month_Year(PL.iloc[row_i,col_i])   
-    st.write("month_table",month_table,year_table)
+    st.write("PL",PL,"month_table",month_table,year_table)
     year_count=[]        
     month_count=[]
     max_len=0
