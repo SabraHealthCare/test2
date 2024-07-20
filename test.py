571e1cf2553e18641e2310e2a4df65b84b3e98d2
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-07-20 16:51:35 -0700

    Update test.py

--- test.py
+++ test.py
@@ -746,7 +746,7 @@ def Identify_Month_Row(PL,sheet_name,pre_date_header,tenantAccount_col_no):
 	# search "current month" as reporting month
 
         if len(valid_col_index) > 1 or len(valid_col_index) ==0:
-            #st.write("valid_col_index",valid_col_index,"valid_col_mask",valid_col_mask)
+            st.write("valid_col_index",valid_col_index,"valid_col_mask",valid_col_mask)
             st.error("Failed to identify any month/year header in sheet: '{}', please add the month/year header and re-upload.".format(sheet_name))
             st.stop()
         elif len(valid_col_index) == 1:  #  only one column contain numeric data
