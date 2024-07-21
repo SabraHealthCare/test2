4093c80567813c9bd4bdeeb70a0ad47cb8391988
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-07-20 19:25:50 -0700

    Update test.py

--- test.py
+++ test.py
@@ -753,7 +753,7 @@ def Identify_Month_Row(PL,sheet_name,pre_date_header,tenantAccount_col_no):
                 column = PL.iloc[0:first_tenant_account_row, col_i]
                 if column.astype(str).str.contains('current month', case=False, na=False).any():
                     current_month_cols.append(col_i)
-                    current_month_rows = column.index[column.astype(str).str.contains('current month', case=False, na=False)].tolist()
+                    current_month_rows = column.index[column.astype(str).str.contains('current month', case=False, na=False)][0]
             if len(current_month_cols)==1:
                 PL_date_header = [0] * PL.shape[1]
                 PL_date_header[current_month_cols[0]] = reporting_month
