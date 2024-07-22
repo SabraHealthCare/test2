837332066836a8ed88a33762219d95d3a69af04d
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-07-22 09:00:11 -0700

    Update test.py

--- test.py
+++ test.py
@@ -650,6 +650,7 @@ def Identify_Month_Row(PL,sheet_name,pre_date_header,tenantAccount_col_no):
         month_sort_index = np.argsort(np.array(month_count))
         for month_index_i in range(-1,-10,-1): 
             st.write("month_sort_index",month_sort_index)
+            st.write("month_sort_index[-1]",month_sort_index[-1])
             month_row_index=month_sort_index[month_index_i]
             month_row=list(month_table.iloc[month_row_index,])
             month_list=list(filter(lambda x:x!=0,month_row))
