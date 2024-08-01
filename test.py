648707f986fc1611c568a9d3a368ca2ac0c9238e
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-08-01 09:53:14 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1993,7 +1993,7 @@ elif st.session_state["authentication_status"] and st.session_state["operator"]!
         if st.button(f'Confirm and upload {operator} {reporting_month[4:6]}-{reporting_month[0:4]} reporting', key='reporting_month', help="Click to confirm and upload"):
             st.session_state.clicked['submit_report'] = True
             st.experimental_set_query_params(clicked=True)
-            #st.experimental_rerun()
+            st.experimental_rerun()
         # Perform the upload action here and check for discrepancies
         if st.session_state.clicked['submit_report']:
             Submit_Upload_Latestmonth()
