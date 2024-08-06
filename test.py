4647d6eb81f4dc1661da1b41f7350271ce58e7bf
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-08-05 21:56:49 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1852,21 +1852,20 @@ elif st.session_state["authentication_status"] and st.session_state["operator"]!
         elif BS_separate_excel=="Y":	 
             with st.form("upload_form", clear_on_submit=True):
                 st.subheader("Select reporting month:") 
-                col1,col2,col3=columns([1,1,1])
+                col1,col2,col3=st.columns([1, 1, 1])
                 with col1:
-                    with col1:
-                        selected_year = st.selectbox("Year", years_range,index=years_range.index(st.session_state.selected_year))
-                    with col2:    
+                    selected_year = st.selectbox("Year", years_range,index=years_range.index(st.session_state.selected_year))
+                with col2:    
                         selected_month = st.selectbox("Month", months_range,index=months_range.index(st.session_state.selected_month))
-                    with col1:
-                        st.subheader("Upload P&L:")
-                        uploaded_finance=st.file_uploader("P&L for monthly reporting. Only XLSX accepted",type={"xlsx"},accept_multiple_files=False,key="Finance_upload")
-                    with col2:
-                        st.subheader("Upload Balance Sheet:")
-                        uploaded_BS=st.file_uploader("",type={"xlsx"},accept_multiple_files=False,key="BS_upload")
-                    with col3:
-                        st.subheader("Other Documents:")
-                        uploaded_other_docs=st.file_uploader("Optional",type=["csv","pdf","xlsm","xlsx","xls"],accept_multiple_files=True,key="Other docs")
+                with col1:
+                    st.subheader("Upload P&L:")
+                    uploaded_finance=st.file_uploader("P&L for monthly reporting. Only XLSX accepted",type={"xlsx"},accept_multiple_files=False,key="Finance_upload")
+                with col2:
+                    st.subheader("Upload Balance Sheet:")
+                    uploaded_BS=st.file_uploader("",type={"xlsx"},accept_multiple_files=False,key="BS_upload")
+                with col3:
+                    st.subheader("Other Documents:")
+                    uploaded_other_docs=st.file_uploader("Optional",type=["csv","pdf","xlsm","xlsx","xls"],accept_multiple_files=True,key="Other docs")
                 submitted = st.form_submit_button("Upload")
                 if submitted:
 	            # clear cache for every upload
