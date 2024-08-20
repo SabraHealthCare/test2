c72cae030bf643600da3d4d21050823248bbba1b
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-08-20 09:57:54 -0700

    Update test.py

--- test.py
+++ test.py
@@ -392,7 +392,7 @@ def Identify_Tenant_Account_Col(PL,sheet_name,sheet_type_name,account_pool,pre_m
 	    
         # Count the number of matches between the candidate column and the account pool
         match_count = sum(candidate_col.isin(account_pool))
-        st.write("match_count",match_count,"non_empty_col",non_empty_col)
+        #st.write("match_count",match_count,"non_empty_col",non_empty_col)
         # Check if the proportion of matches exceeds 30% of all non-empty values
         if match_count>0 and match_count / len(non_empty_col) > 0.3:
             if len(pre_max_match_col)==1:
