a3a4cbfd3e7a90f0d7d37ca9d757522e2862a48b
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-08-27 15:20:01 -0700

    Update test.py

--- test.py
+++ test.py
@@ -385,6 +385,7 @@ def Identify_Tenant_Account_Col(PL, sheet_name, sheet_type_name, account_pool, p
     # Helper function to clean and match candidate columns
     if sheet_name=="Hours":
         st.write("PL",PL)
+        st.write("account_pool",account_pool)
     def get_match_count(col_index):
         candidate_col = PL.iloc[:, col_index].fillna('').astype(str).str.strip().str.upper()
         non_empty_col = candidate_col[candidate_col != '']
