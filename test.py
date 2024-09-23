3089593078747c7b542b56e89423af8e68852123
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-09-23 09:23:50 -0700

    Update test.py

--- test.py
+++ test.py
@@ -283,7 +283,7 @@ def Initial_Mapping(operator):
     st.write("account_mapping1",account_mapping)
     # Clean and format account mapping columns
     account_mapping_cols = ["Sabra_Account", "Sabra_Second_Account", "Tenant_Account"]
-
+    st.write("BPC_Account",BPC_Account)
     account_mapping[account_mapping_cols] = account_mapping[account_mapping_cols].applymap(lambda x: x.upper().strip() if pd.notna(x) else x)
     account_mapping=account_mapping.merge(BPC_Account[["BPC_Account_Name","Category"]], left_on="Sabra_Account",right_on="BPC_Account_Name",how="left").drop(columns="BPC_Account_Name")
     st.write("account_mapping2",account_mapping)
