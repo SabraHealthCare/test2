139fa485214e5a217372e6cd5814e4bf3ad491dc
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-09-23 09:44:45 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1154,7 +1154,7 @@ def View_Summary():
         st.write("")
         st.write("reporting_month_data",reporting_month_data)
         summary_for_email= reporting_month_data[reporting_month_data["Sabra_Account"].isin(["Total - Revenue", "Total - Operating Expenses", "Total - Non-Operating Expenses"])][["Sabra_Account","Total"]+list(entity_columns)]
-        summary_for_email= reporting_month_data.astype(int)
+        summary_for_email = reporting_month_data.assign( **reporting_month_data.drop(columns=["Sabra_Account"]).astype(int))
         summary_for_email.columns.name = None 
 
         email_body=f"<p>Here is the summary for your reference:</p>{summary_for_email.to_html(index=False)}"+email_body
