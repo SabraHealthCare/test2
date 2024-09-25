0a83a3c8817b6870df4428913545a0b5128e25a6
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-09-25 11:11:58 -0700

    Update test.py

--- test.py
+++ test.py
@@ -282,6 +282,7 @@ def Initial_Mapping(operator):
     account_mapping[account_mapping_cols] = account_mapping[account_mapping_cols].applymap(lambda x: x.upper().strip() if pd.notna(x) else x)
     account_mapping=account_mapping.merge(BPC_Account[["BPC_Account_Name","Category"]], left_on="Sabra_Account",right_on="BPC_Account_Name",how="left").drop(columns="BPC_Account_Name")
     account_mapping = account_mapping[["Operator", "Sabra_Account", "Sabra_Second_Account", "Tenant_Account", "Conversion","Category"]]
+    st.write("account_mapping",account_mapping)
     entity_mapping=Read_File_From_Onedrive(mapping_path,entity_mapping_filename,"CSV",entity_mapping_str_col)
     entity_mapping = (Read_File_From_Onedrive(mapping_path, entity_mapping_filename, "CSV", entity_mapping_str_col)
                   .reset_index(drop=True)
