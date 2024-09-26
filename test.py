e9030112b636f3c4e27d574f677f17238feb2da4
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-09-25 20:18:28 -0700

    Update test.py

--- test.py
+++ test.py
@@ -363,16 +363,19 @@ def ChangeWidgetFontSize(wgt_txt, wch_font_size = '12px'):
 # Parse the df and get filter widgets based for provided columns
 		
 def Identify_Tenant_Account_Col(PL, sheet_name, sheet_type_name, account_pool, pre_max_match_col):
+    st.write("sheet_name",sheet_name)
     def get_match_count(col_index):
         candidate_col = PL.iloc[:, col_index].fillna('').astype(str).str.strip().str.upper()
         non_empty_col = candidate_col[candidate_col != '']
         match_count = sum(candidate_col.isin(account_pool))
+        st.write("candidate_col,match_count",candidate_col,match_count)
         return match_count, len(non_empty_col)
     
     # Check the pre-identified columns first
     if pre_max_match_col != [10000] and pre_max_match_col[0] < PL.shape[1] and len(pre_max_match_col)==1:
         for i in range(len(pre_max_match_col)):
             match_count, non_empty_count = get_match_count(pre_max_match_col[i])
+            st.write("error")
             if match_count > 0 and (match_count > 1 or match_count / non_empty_count > 0.2):
                 if i == len(pre_max_match_col) - 1:
                     return pre_max_match_col
@@ -381,6 +384,7 @@ def Identify_Tenant_Account_Col(PL, sheet_name, sheet_type_name, account_pool, p
     match_counts = []
     for col in range(min(15, PL.shape[1])):
         match_count, _ = get_match_count(col)
+        st.write("match_count",match_count)
         match_counts.append((match_count, col))
     
     # Sort by match count in descending order
