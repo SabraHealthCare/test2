94b11ab49c973dd3ad1b6cdd3939c4e241d8a887
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-10-08 15:34:52 -0700

    Update test.py

--- test.py
+++ test.py
@@ -274,9 +274,9 @@ def Initial_Mapping(operator):
     #st.write("account_mapping_all",account_mapping_all)
     # Handle case where there's only one row and it corresponds to a template
     account_mapping = account_mapping_all[account_mapping_all["Operator"]==operator]
-    st.write("template",account_mapping_all[account_mapping_all["Operator"] == "Template"])
-    st.write(account_mapping["Sabra_Account"].values[0])
-    if account_mapping.shape[0] == 1 and account_mapping["Sabra_Account"].values[0] == 'Template':
+    #st.write("template",account_mapping_all[account_mapping_all["Operator"] == "Template"])
+    #st.write(account_mapping["Sabra_Account"].values[0])
+    if account_mapping.shape[0] == 1 and account_mapping["Sabra_Account"].values[0] == 'TEMPLATE':
         account_mapping = account_mapping_all[account_mapping_all["Operator"] == "Template"].copy()
         account_mapping["Operator"] = operator	
     
