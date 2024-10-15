bf4ec699849053ca62773ae82cf8f2346ec17944
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-10-15 14:04:30 -0700

    Update test.py

--- test.py
+++ test.py
@@ -279,7 +279,7 @@ def Initial_Mapping(operator):
         account_mapping = account_mapping_all[account_mapping_all["Operator"] == "Template"].copy()
         account_mapping["Operator"] = operator	
     
-    st.write("account_mapping1",account_mapping)  
+    #st.write("account_mapping1",account_mapping)  
     # Clean and format account mapping columns
     account_mapping_cols = ["Sabra_Account", "Sabra_Second_Account", "Tenant_Account"]
     account_mapping[account_mapping_cols] = account_mapping[account_mapping_cols].applymap(lambda x: x.upper().strip() if pd.notna(x) else x)
@@ -293,7 +293,7 @@ def Initial_Mapping(operator):
                   .query("Operator == @operator")
                   .set_index("ENTITY"))
     entity_mapping[["DATE_ACQUIRED", "DATE_SOLD_PAYOFF"]] = entity_mapping[["DATE_ACQUIRED", "DATE_SOLD_PAYOFF"]].astype(str)  
-    st.write("entity_mapping",entity_mapping)
+    #st.write("entity_mapping",entity_mapping)
     return BPC_pull,entity_mapping,account_mapping
 
 	
