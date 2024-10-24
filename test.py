1a8b60a095d5225a87665e24c2912f946096b7d8
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-10-24 10:08:27 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1786,7 +1786,7 @@ def Read_Clean_PL_Single(entity_i,sheet_type,uploaded_file,account_pool):
         #st.write("process PL",PL)    
         # filter columns with month_select
         st.write("date_header[0]",date_header[0],"select_months_list",select_months_list)
-        selected_month_columns = [val in select_months_list for val in date_header[0]]
+        selected_month_columns = [val in select_months_list or val==reporting_month for val in date_header[0]]
         st.write("selected_month_columns",selected_month_columns)
         PL = PL.loc[:,selected_month_columns]   
         PL.columns= [value for value in date_header[0] if value in select_months_list]        
