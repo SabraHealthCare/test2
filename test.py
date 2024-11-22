2dff8d569567afe86c988cd02ca9ec391739773e
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-11-22 15:43:43 -0800

    Update test.py

--- test.py
+++ test.py
@@ -761,7 +761,7 @@ def Identify_Month_Row(PL,tenant_account_col_values,tenantAccount_col_no,sheet_n
                         #st.write("max_match_year",max_match_year,"year_table",year_table)
                         PL_date_header=year_table.iloc[month_row_index,].apply(lambda x:str(int(x)))+\
                                                       month_table.iloc[month_row_index,].apply(lambda x:"" if x==0 else "0"+str(int(x)) if x<10 else str(int(x)))
-                        #st.write("sheet_name",sheet_name,"PL_date_header1",PL_date_header,"reporting_month",reporting_month)
+                        
 		        
                         if reporting_month not in list(PL_date_header):
                             #year_table.iloc[month_row_index,]=Fill_Year_To_Header(list(month_table.iloc[month_row_index,]),sheet_name,reporting_month)
