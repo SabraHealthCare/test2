2c0a72e56b2801fa5a5b71e44594f7be146468f9
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2024-12-19 12:15:56 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1307,8 +1307,10 @@ def Submit_Upload(total_email_body):
     # Get 'Asset_Manager' from entity_mapping
     unique_asset_managers = entity_mapping['Asset_Manager'].unique()
     #receiver_email_list=[operator_email,"twarner@sabrahealth.com","sli@sabrahealth.com"]
+
     st.write("*************************************operator_email",operator_email)
-    receiver_email_list=[operator_email,"sli@sabrahealth.com"]
+    #receiver_email_list=[operator_email,"sli@sabrahealth.com"]
+    receiver_email_list=operator_email.split(",") + ["sli@sabrahealth.com"]
     if '@*' in operator_email:
         st.write("Please update email address (in 'Menu' - 'Edit Account') to ensure you receive confirmation email.")
     # Append these unique values to receiver_list
