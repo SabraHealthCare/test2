1b6ff3e01e5e6d75466e111e5ffaaa07cc0ac13e
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2025-01-10 16:25:55 -0800

    Update test.py

--- test.py
+++ test.py
@@ -283,10 +283,12 @@ def Initial_Mapping(operator):
     # Clean and format account mapping columns
     account_mapping_cols = ["Sabra_Account", "Sabra_Second_Account"]
     account_mapping[account_mapping_cols] = account_mapping[account_mapping_cols].applymap(lambda x: x.upper().strip() if pd.notna(x) else x)
+    st.write("account_mapping2",account_mapping)  
     account_mapping["Tenant_Account"] = account_mapping["Tenant_Account"].apply(lambda x: str(int(x)).strip().upper() if pd.notna(x) and isinstance(x, float) else (str(x).strip().upper() if pd.notna(x) else x))
     account_mapping=account_mapping.merge(BPC_Account[["BPC_Account_Name","Category"]], left_on="Sabra_Account",right_on="BPC_Account_Name",how="left").drop(columns="BPC_Account_Name")
+    st.write("account_mapping3",account_mapping)  
     account_mapping = account_mapping[["Operator", "Sabra_Account", "Sabra_Second_Account", "Tenant_Account", "Conversion","Category"]]
-    #st.write("account_mapping",account_mapping)  
+    st.write("account_mapping4",account_mapping)  
 
     entity_mapping = (Read_File_From_Onedrive(mapping_path, entity_mapping_filename, "CSV", entity_mapping_str_col)
                   .reset_index(drop=True)
