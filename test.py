f173afb68b7e71ff0261a5bebbc624ac91cefa75
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2025-01-28 14:38:05 -0800

    Update test.py

--- test.py
+++ test.py
@@ -1775,9 +1775,9 @@ def Read_Clean_PL_Multiple(entity_list,sheet_type,uploaded_file,account_pool,she
                 account_pool=account_mapping.copy()
             elif sheet_type=="Sheet_Name_Occupancy":
                 account_pool = account_mapping[(account_mapping["Sabra_Account"] == "NO NEED TO MAP")|\
-		                (account_mapping["Category"].isin(["Facility Information","Patient Days","Operating Expenses","Labor Expenses"]))|\
-	                        (account_mapping["Sabra_Account"].isin(['T_NURSING_HOURS', 'T_N_CONTRACT_HOURS', 'T_OTHER_HOURS'])) |\
-	                        (account_mapping["Sabra_Second_Account"].isin(['T_NURSING_HOURS', 'T_N_CONTRACT_HOURS', 'T_OTHER_HOURS']))]	  
+		                (account_mapping["Category"].isin(["Facility Information","Patient Days","Operating Expenses"]))|\
+	                        (account_mapping["Sabra_Account"].isin(['T_NURSING_HOURS', 'T_N_CONTRACT_HOURS', 'T_OTHER_HOURS','T_NURSING_LABOR','T_N_CONTRACT_LABOR','T_OTHER_NN_LABOR'])) |\
+	                        (account_mapping["Sabra_Second_Account"].isin(['T_NURSING_HOURS', 'T_N_CONTRACT_HOURS', 'T_OTHER_HOURS','T_NURSING_LABOR','T_N_CONTRACT_LABOR','T_OTHER_NN_LABOR']))]	  
             elif sheet_type=="Sheet_Name_Balance_Sheet":
                 account_pool= account_mapping[(account_mapping["Sabra_Account"] == "NO NEED TO MAP")| (account_mapping["Category"]=="Balance Sheet")]	
 
