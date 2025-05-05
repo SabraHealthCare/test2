50f4db42ef7dbf532c0c67d10faca450e2659662
Monthly-Reporting <87496388+SabraHealthCare@users.noreply.github.com>
2025-05-05 15:45:45 -0700

    Update test.py

--- test.py
+++ test.py
@@ -342,7 +342,7 @@ def Initial_Mapping(operator):
     account_mapping_all = Read_File_From_Onedrive(mapping_path,account_mapping_filename,"XLSX",account_mapping_str_col)
     #st.write("account_mapping_filename",account_mapping_filename,"mapping_path",mapping_path,"account_mapping_all",account_mapping_all)    
     # Handle case where there's only one row and it corresponds to a template
-    account_mapping = account_mapping_all[account_mapping_all["Operator"]==operator]
+    account_mapping = account_mapping_all[account_mapping_all["Operator"]==operator].copy()
     #st.write(account_mapping)
     #st.write(account_mapping["Sabra_Account"].values[0])
     if account_mapping.shape[0] == 1 and account_mapping["Sabra_Account"].values[0] == 'TEMPLATE':
